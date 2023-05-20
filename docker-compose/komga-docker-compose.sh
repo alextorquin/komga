7b653c2a16597d488a10f326c4e14d6c887f11c6
alejandro.torrado.udesa <alejandro.torrado@udesa.net>
2023-05-21 01:39:25 +0200

    .

--- docker-compose/komga-docker-compose.sh
+++ docker-compose/komga-docker-compose.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir /var/data
+mkdir /var/data/komga
 mkdir /var/data/komga/config
 mkdir /var/data/komga/data
 mkdir /var/data/komga/media
--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Este script sobreescribirá cualquier cambio que se haya efectuado en este proyecto udesa-tools. Esta ruta está pensada pra PROducción, por lo que no se esperan cambios que deban ser integrados en control de versiones."
+echo "Este script sobreescribirá cualquier cambio que se haya efectuado en este proyecto. Esta ruta está pensada pra PROducción, por lo que no se esperan cambios que deban ser integrados en control de versiones."
 
 read -p " ¿Estás seguro de continuar? (S/N)" yn
 case $yn in
