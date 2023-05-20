b9b7f857d73bd2cb032d33beb5d57141a713af4d
alejandro.torrado.udesa <alejandro.torrado@udesa.net>
2023-05-21 00:50:11 +0200

    Se añade script de actualización automática

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Este script sobreescribirá cualquier cambio que se haya efectuado en este proyecto udesa-tools. Esta ruta está pensada pra PROducción, por lo que no se esperan cambios que deban ser integrados en control de versiones."
+
+read -p " ¿Estás seguro de continuar? (S/N)" yn
+case $yn in
+    s|S )
+        git restore *
+        git pull
+        sudo chmod -R +x ./
+    ;;
+    * )
+        echo "Fin del script. No se han realizado cambios."
+    ;;
+esac
